c765be67400d85673f70862b006fe71574a5cefb
aacm13 <00354817@uca.edu.sv>
2023-11-29 14:23:00 -0600

    prisimic revalidation

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,8 +1,26 @@
 import { NextResponse } from "next/server";
 import { revalidateTag } from "next/cache";
 
-export async function POST() {
-  revalidateTag("prismic");
 
-  return NextResponse.json({ revalidated: true, now: Date.now() });
+export async function POST(request) {
+  const { secret } = await request.json();
+  const tag = request.headers.get('tag');
+
+  if (secret !== process.env.REVALIDATION_TOKEN) {
+    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
+  }
+
+  if (!tag) {
+    return NextResponse.json(
+      { message: "Missing tag header" },
+      { status: 400 }
+    )
+  }
+
+  revalidateTag(tag);
+
+  return NextResponse.json(
+    { revalidated: true, now: Date.now() },
+    { status: 200 }
+  );
 }
