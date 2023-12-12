e0acb46f3ac53d12bcf052f0892d63e7dcc8be0d
aacm13 <00354817@uca.edu.sv>
2023-12-11 20:12:04 -0600

    returning to previous revalidation

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,47 +1,25 @@
-// pages/api/revalidate.js
+import { NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
 
-import * as prismic from '@prismicio/client'
-import * as prismicH from '@prismicio/helpers'
+export async function POST(request) {
+  const { secret } = await request.json();
+  const tag = request.headers.get('tag');
 
-// Import your app's Link Resolver (if your app uses one)
-
-
-/**
- * This API endpoint will be called by a Prismic webhook. The webhook
- * will send an object containing a list of added, updated, or deleted
- * documents. Pages for those documents will be rebuilt.
- *
- * The Prismic webhook must send the correct secret.
- */
-export default async function handler(req, res) {
-  if (req.body.type === 'api-update' && req.body.documents.length > 0) {
-    // Check for secret to confirm this is a valid request
-    if (req.body.secret !== process.env.REVALIDATION_TOKEN) {
-      return res.status(401).json({ message: 'Invalid token' })
-    }
-
-    // If you have a `createClient()` function defined elsewhere in
-    // your app, use that instead
-    const client = prismic.createClient()
-
-    // Get a list of URLs for any new, updated, or deleted documents
-    const documents = await client.getAllByIDs(req.body.documents)
-    const urls = documents.map((doc) => prismicH.asLink(doc))
-
-    try {
-      // Revalidate the URLs for those documents
-      await Promise.all(
-        urls.map(async (url) => await res.revalidate(url))
-      )
-
-      return res.json({ revalidated: true })
-    } catch (err) {
-      // If there was an error, Next.js will continue to show
-      // the last successfully generated page
-      return res.status(500).send('Error revalidating')
-    }
+  if (secret !== process.env.REVALIDATION_TOKEN) {
+    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
   }
 
-  // If the request's body is unknown, tell the requester
-  return res.status(400).json({ message: 'Invalid body' })
+  if (!tag) {
+    return NextResponse.json(
+      { message: "Missing tag header" },
+      { status: 400 }
+    )
+  }
+
+  revalidateTag(tag);
+
+  return NextResponse.json(
+    { revalidated: true, now: Date.now() },
+    { status: 200 }
+  );
 }
